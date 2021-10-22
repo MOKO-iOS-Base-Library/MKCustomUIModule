830428b1188c0508350b748c90966cc78f98ff80
lovexiaoxia <aadyx2007@163.com>
2021-10-22 14:30:24 +0800

    去掉dfu功能
    Signed-off-by: lovexiaoxia <aadyx2007@163.com>

--- Example/Pods/Target Support Files/Pods-MKCustomUIModule_Example/Pods-MKCustomUIModule_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-MKCustomUIModule_Example/Pods-MKCustomUIModule_Example-frameworks.sh
@@ -184,8 +184,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Toast/Toast.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WRNavigationBar/WRNavigationBar.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/ZIPFoundation/ZIPFoundation.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/iOSDFULibrary/iOSDFULibrary.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CircleProgressBar/CircleProgressBar.framework"
@@ -197,8 +195,6 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Toast/Toast.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WRNavigationBar/WRNavigationBar.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/ZIPFoundation/ZIPFoundation.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/iOSDFULibrary/iOSDFULibrary.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- MKCustomUIModule.podspec
+++ MKCustomUIModule.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'MKCustomUIModule'
-  s.version          = '1.3.1'
+  s.version          = '1.3.2'
   s.summary          = '通用UI组件库'
 
 # This description is used to generate tags and improve search results.
@@ -191,8 +191,6 @@ TODO: Add long description of the pod here.
             
             ssss.dependency 'MKCustomUIModule/Controller/MKAboutPage/Model'
           end
-          
-          sss.dependency 'iOSDFULibrary'
       end
     
   end
